c5bf6210f02b5341fdfc812ea635ee006ffe8290
Kadriye Taylan <kdryetyln@gmail.com>
2015-03-20 02:36:13 +0200

    Create class1.cpp

---
+++ class1.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+using namespace std;
+class samp
+{
+    int i;
+public:
+    samp(int n)
+    {
+        i=n;
+        cout<<"olusturuluyor ..."<<endl;
+    }
+    ~samp()
+    {
+        cout<<"yok ediliyor ..."<<endl;
+
+    }
+    int goster()
+        {
+            return i;
+        }
+};
+int kopya(samp j)//kopya burada olusturulmus oldu
+{
+    return j.goster()*j.goaster();
+}
+int main()
+{
+   samp a(10);
+   cout << kopya(a) << endl;
+    return 0;
+}
