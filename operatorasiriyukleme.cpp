9d00466dae8934a89c5917ababdc926b56f1bac9
Kadriye Taylan <kdryetyln@gmail.com>
2015-11-13 18:29:22 +0200

    Update operatorasiriyukleme.cpp

--- operatorasiriyukleme.cpp
+++ operatorasiriyukleme.cpp
@@ -12,25 +12,59 @@ public:
     }
     toplama(int,int);
     toplama operator+(toplama);
+    toplama operator=(toplama);
+    toplama operator-(toplama);
+    toplama operator+(int m);
 };
+
+toplama toplama::operator+(int m)
+{
+    toplama gecici;
+    gecici.x=x+m;
+    gecici.y=y+m;
+    return gecici;
+}
+
 toplama::toplama(int a,int b)
 {
     x=a; y=b;
 }
-toplama toplama::operator+(toplama koord)
+toplama toplama::operator+(toplama sayi)
 {
     toplama gecici;
-    gecici.x=x+koord.x;
-    gecici.y=y+koord.y;
+    gecici.x=x+sayi.x;
+    gecici.y=y+sayi.y;
     return gecici;
 }
-
+toplama toplama::operator-(toplama sayi)
+{
+    toplama gecici;
+    gecici.x=x-sayi.x;
+    gecici.y=y-sayi.y;
+    return gecici;
+}
+toplama toplama::operator=(toplama sayi)
+{
+    x=sayi.x;
+    y=sayi.y;
+    return *this;
+}
 int main()
 {
     toplama a(19,23);
     toplama b(5,4);
     toplama c;
     c=a+b;//2 nesneyi topladik.  + isleci operator+ yı cagirdi
-    cout << "1. sayi : "<<c.x<<endl<<"2. sayi : "<<c.y;
+    cout << "1. sayi : "<<c.x<<"    2. sayi : "<<c.y<<endl<<endl;
+    c=a-b;//2nesneyi cikardik . - isleci operator- yi cagirdi
+    cout << "1. sayi : "<<c.x<<"    2. sayi : "<<c.y<<endl<<endl;
+    c=a;//c ye a nesnesini esitledik.  = operatoru  operator= i cagirdi
+    cout << "1. sayi : "<<c.x<<"    2. sayi : "<<c.y<<endl<<endl;
+    c=b;//c ye a nesnesini esitledik.  = operatoru  operator= i cagirdi
+    cout << "1. sayi : "<<c.x<<"     2. sayi : "<<c.y<<endl<<endl;
+    c=a+10;//buradaki + islecide operator+(int m ) i bize cagiracaktir
+    cout << "1. sayi : "<<c.x<<"    2. sayi : "<<c.y<<endl<<endl;
+    c=b+25;//buradaki + islecide operator+(int m ) i bize cagiracaktir
+    cout << "1. sayi : "<<c.x<<"    2. sayi : "<<c.y<<endl<<endl;
     return 0;
 }
